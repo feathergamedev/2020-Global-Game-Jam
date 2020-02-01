137cc66a80a4f9979852e8b30748132c28d42bb7
Li TsungYi <litsungyi@gmail.com>
2020-02-01 13:28:18 +0800

    Add Events

---
+++ 2020-Global-Game-Jam/Assets/Script/Infrastructures/Events/EventEmitter.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repair.Infrastructures
+{
+    public static class EventEmitter
+    {
+        private static Dictionary<string, List<Action<object>>> listeners = new Dictionary<string, List<Action<object>>>();
+
+        public static void Reset()
+        {
+            listeners.Clear();
+        }
+
+        public static void Add(string eventName, Action<object> action)
+        {
+            if (listeners.ContainsKey(eventName))
+            {
+                listeners[eventName].Add(action);
+            }
+            else
+            {
+                listeners[eventName] = new List<Action<object>>
+                {
+                    action
+                };
+            } 
+        } 
+
+        public static void Remove(string eventName, Action<object> action)
+        {
+            if (listeners.ContainsKey(eventName))
+            {
+                listeners[eventName].Remove(action);
+                if (!listeners[eventName].Any())
+                {
+                    listeners.Remove(eventName);
+                }
+            }
+        }
+
+        public static void Emit(string eventName, object eventParams = null)
+        {
+            if (listeners.ContainsKey(eventName))
+            {
+                foreach (var action in listeners[eventName])
+                {
+                    action?.Invoke(eventParams);
+                }
+            }
+        }
+    }
+}
