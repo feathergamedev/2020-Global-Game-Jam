5e6279da056162d716911e20b7fd460fbbe74a49
Li TsungYi <litsungyi@gmail.com>
2020-02-01 18:01:12 +0800

    Add singleton

---
+++ 2020-Global-Game-Jam/Assets/Script/Infrastructures/Core/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Repair.Infrastructures.Core
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instance;
+
+        public static T I => (instance == null) ? CreateInstance() : instance;
+
+        private static T CreateInstance()
+        {
+            Assert.IsNull(instance);
+
+            var go = new GameObject();
+            go.name = nameof(T);
+            instance = go.AddComponent<T>();
+
+            return instance;
+        }
+    }
+}
